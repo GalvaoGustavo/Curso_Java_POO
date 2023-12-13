c4dee051256e8f81f1602465eb0d790f0ef0a570
GalvaoGustavo <117182269+GalvaoGustavo@users.noreply.github.com>
2023-12-13 08:12:31 -0300

    Add files via upload

---
+++ Operador_Atribuicao/src/operadorAtribuicao/OperadorAtribuicao.java
@@ -0,0 +1,23 @@
+package operadorAtribuicao;
+
+public class OperadorAtribuicao {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		System.out.println("Valor original: "+num);
+		num += 3;
+		System.out.println("Valor número: "+num);
+		num *= 4;
+		System.out.println("Valor numero: "+num);
+		num /= 2;
+		System.out.println("Valor numero: "+num);
+		num %= 2;
+		System.out.println("Valor numero: "+num);
+
+
+
+
+	}
+
+}
